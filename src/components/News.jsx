d5108719ed0b202cec76039fbef7dc94311531ad
d00d1e <lee.julianay@gmail.com>
2021-09-21 14:27:43 -0700

    add crypto news filter

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,35 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 
 import { useGetCryptoNewsQuery } from "../api/cryptoNewsApi";
+import { useGetCryptosQuery } from "../api/cryptoApi";
 
 const altImage =
   "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
 
 export default function News({ simplified }) {
-  const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({
-    newsCategory: "Cryptocurrency",
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+
+  const { data } = useGetCryptosQuery(100);
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory,
     count: simplified ? 6 : 12,
   });
-
-  console.log("News", cryptoNews);
 
   if (!cryptoNews?.value) return "Loading...";
 
   return (
     <Row gutter={[24, 24]}>
-      {cryptoNews?.value?.map((news, i) => (
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Select.Option value="Cryptocurency">Cryptocurrency</Select.Option>
+            {data?.data?.coins?.map((currency) => (
+              <Select.Option value={currency.name}>
+                {currency.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Col>
+      )}
+      {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
-          <Card className="news-card" hoverable>
+          <Card hoverable className="news-card">
             <a href={news.url} target="_blank" rel="noreferrer">
               <div className="news-image-container">
-                <Typography.Title className="news-title" level={4}>
+                <Typography.Text className="news-title" level={4}>
                   {news.name}
-                </Typography.Title>
+                </Typography.Text>
                 <img
                   src={news?.image?.thumbnail?.contentUrl || altImage}
-                  alt="news"
+                  alt=""
                 />
               </div>
               <p>
@@ -43,13 +66,13 @@ export default function News({ simplified }) {
                     src={
                       news.provider[0]?.image?.thumbnail?.contentUrl || altImage
                     }
-                    alt="news"
+                    alt=""
                   />
                   <Typography.Text className="provider-name">
                     {news.provider[0]?.name}
                   </Typography.Text>
                 </div>
-                <Typography.Text className="provider-date">
+                <Typography.Text>
                   {moment(news.datePublished).startOf("ss").fromNow()}
                 </Typography.Text>
               </div>
